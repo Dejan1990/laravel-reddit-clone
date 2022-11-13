7bb593c14158fa538ead582fefac261fb899236b
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-13 12:04:50 +0100

    models and migrations added

---
+++ database/migrations/2022_11_13_104849_create_post_votes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_votes', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->onDelete('cascase');
+            $table->foreignId('post_id')->constrained()->onDelete('cascase');
+            $table->smallInteger('vote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_votes');
+    }
+};
