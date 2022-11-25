022363f4e6c6833e59d0a8a02e1a7654a8036a42
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-25 17:09:37 +0100

    Create SuccessMessage component

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,6 +4,7 @@ import Dropdown from '@/Components/Dropdown.vue'
 import DropdownLink from '@/Components/DropdownLink.vue'
 import NavLink from '@/Components/NavLink.vue'
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue'
+import SuccessMessage from '@/Components/SuccessMessage.vue'
 import { Link } from '@inertiajs/inertia-vue3'
 import { ref } from "vue";
 
@@ -12,20 +13,7 @@ const showingNavigationDropdown = ref(false);
 
 <template>
   <div>
-    <div v-if="$page.props.flash.message" class="bg-green-400">
-        <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-            <div class="flex items-center justify-between flex-wrap">
-                <div class="w-0 flex-1 flex items-center">
-                    <p class="ml-3 font-medium text-white truncate">
-                    <span class="md:hidden"> {{ $page.props.flash.message }} </span>
-                    <span class="hidden md:inline">
-                        {{ $page.props.flash.message }}
-                    </span>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <SuccessMessage />
     <div class="min-h-screen bg-gray-100">
       <nav class="bg-white border-b border-gray-100">
         <!-- Primary Navigation Menu -->
