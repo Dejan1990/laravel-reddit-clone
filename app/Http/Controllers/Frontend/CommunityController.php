a51f0670d3378fb488d24a127072b7d211c94055
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-27 13:30:15 +0100

    Display posts with pagination

--- app/Http/Controllers/Frontend/CommunityController.php
+++ app/Http/Controllers/Frontend/CommunityController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use Inertia\Inertia;
 use App\Models\Community;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommunityPostResource;
 
 class CommunityController extends Controller
 {
     public function show($slug)
     {
+        $community = Community::where('slug', $slug)->first();
+        $posts = CommunityPostResource::collection($community->posts()->with('user')->paginate(3));
+
         return Inertia::render('Frontend/Communities/Show', [
-            'community' => Community::where('slug', $slug)->first()
+            'community' => $community,
+            'posts' => $posts
         ]);
     }
 }
--- tests/Feature/FrontendCommunityControllerTest.php
+++ tests/Feature/FrontendCommunityControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Community;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,7 +14,16 @@ class FrontendCommunityControllerTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function frontendCommunityShowProperly()
+    public function unauthenticateUserCanVisitfrontendCommunityPage()
+    {
+        $community = Community::factory()->create();
+
+        $this->get('/r/' . $community->slug)
+            ->assertOk();
+    }
+
+    /** @test */
+    public function frontendCommunityShowsProperly()
     {
         $community = Community::factory()->create();
 
@@ -28,5 +38,37 @@ class FrontendCommunityControllerTest extends TestCase
                     'community.user_id' => $community->user->id,
                 ])
         );
+    }
+
+    /** @test */
+    public function frontendCommunityShowsPostsProperly()
+    {
+        $community = Community::factory()->create();
+        Post::factory(5)->for($community)->create();
+
+        $this->get('/r/' . $community->slug)
+            ->assertOk()
+            ->assertInertia(fn ($page) => $page
+                ->has('posts.data', 3)
+        );
+    }
+
+    /** @test */
+    public function itListsOnlyCommunityPostsInfoThatWeExpect()
+    {
+        $community = Community::factory()->create();
+        $posts = Post::factory(5)->for($community)->create();
+
+        $this->get('/r/' . $community->slug)
+            ->assertOk()
+            ->assertInertia(fn ($page) => $page
+                ->hasAll([
+                    'posts.data.0.id',
+                    'posts.data.0.slug',
+                    'posts.data.0.title',
+                    'posts.data.0.description',
+                    'posts.data.0.username'
+                ])
+            );
     }
 }
