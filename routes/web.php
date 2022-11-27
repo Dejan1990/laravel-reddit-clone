fcb58f965d4a1f777bc62f3a724160042f7155f0
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-27 16:24:20 +0100

    Display post

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use App\Http\Controllers\Backend\CommunityController;
 use App\Http\Controllers\Backend\CommunityPostController;
 use App\Http\Controllers\Frontend\CommunityController as FrontendCommunityController;
+use App\Http\Controllers\Frontend\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +28,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/r/{slug}', [FrontendCommunityController::class, 'show'])->name('frontend.community.show');
+Route::get('/r/{slug}', [FrontendCommunityController::class, 'show'])
+    ->name('frontend.community.show');
+Route::get('/r/{community_slug}/posts/{post:slug}', [PostController::class, 'show'])
+    ->name('frontend.communities.posts.show');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/dashboard', function () {
---
+++ tests/Feature/FrontendPostControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Community;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FrontendPostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function unauthenticateUserCanVisitfrontendPostShowPage()
+    {
+        $community = Community::factory()->create();
+        $post = Post::factory()->create();
+
+        $this->get(route('frontend.communities.posts.show', [$community->slug, $post->slug]))
+            ->assertOk();
+    }
+
+    /** @test */
+    public function frontendPostShowsProperly()
+    {
+        $community = Community::factory()->create();
+        $post = Post::factory()->for($community)->create();
+
+        $this->get(route('frontend.communities.posts.show', [$community->slug, $post->slug]))
+            ->assertOk()
+            ->assertInertia(fn ($page) => $page
+                ->hasAll('post', 'community')
+                ->where('post.community_id', $community->id)
+        );
+    }
+}
