203286354eff8ec43e0e5a3c9c3b2a5bea0f2bca
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-12-02 15:44:29 +0100

    Design show post page

--- app/Http/Controllers/Frontend/PostController.php
+++ app/Http/Controllers/Frontend/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostShowResource;
 use App\Models\Community;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ class PostController extends Controller
     {
         return Inertia::render('Frontend/Posts/Show', [
             'community' => $community,
-            'post' => $post
+            'post' => new PostShowResource($post)
         ]);
     }
 }
--- tests/Feature/FrontendPostControllerTest.php
+++ tests/Feature/FrontendPostControllerTest.php
@@ -32,7 +32,27 @@ class FrontendPostControllerTest extends TestCase
             ->assertOk()
             ->assertInertia(fn ($page) => $page
                 ->hasAll('post', 'community')
-                ->where('post.community_id', $community->id)
+        );
+    }
+
+    /** @test */
+    public function itListsOnlyPostsInfoThatWeExpect()
+    {
+        $community = Community::factory()->create();
+        $post = Post::factory()->for($community)->create();
+
+        $this->get(route('frontend.communities.posts.show', [$community->slug, $post->slug]))
+            ->assertOk()
+            ->assertInertia(fn ($page) => $page
+                ->whereAll([
+                    'post.data.id' => $post->id,
+                    'post.data.slug' => $post->slug,
+                    'post.data.title' => $post->title,
+                    'post.data.url' => $post->url,
+                    'post.data.description' => $post->description,
+                    'post.data.username' => $post->user->username
+                ])
+                ->missing('community_id')
         );
     }
 }
